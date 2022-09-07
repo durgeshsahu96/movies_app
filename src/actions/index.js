ee9881ca13350b4f731866cfe30d020a2fc03773
durgesh <durgeshsahu1996@gmail.com>
2022-09-07 17:49:17 +0530

    Add files via upload

---
+++ src/actions/index.js
@@ -0,0 +1,59 @@
+export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_TO_FAVOURITES = "ADD_TO_FAVOURITES";
+export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES";
+export const SET_SHOW_FAVOURITES = "SET_SHOW_FAVOURITES";
+export const ADD_MOVIE_TO_LIST = "ADD_MOVIE_TO_LIST";
+export const ADD_SEARCH_RESULT = "ADD_SEARCH_RESULT";
+
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies,
+  };
+}
+
+export function addToFavourites(movie) {
+  return {
+    type: ADD_TO_FAVOURITES,
+    movie,
+  };
+}
+
+export function setShowFavourites(val) {
+  return {
+    type: SET_SHOW_FAVOURITES,
+    val,
+  };
+}
+
+export function removeFromFavourites(movie) {
+  return {
+    type: REMOVE_FROM_FAVOURITES,
+    movie,
+  };
+}
+
+export function addMovieToList(movie) {
+  return {
+    type: ADD_MOVIE_TO_LIST,
+    movie,
+  };
+}
+
+export function handleMovieSearch(searchText) {
+  return function (dispatch) {
+    const url = `http://www.omdbapi.com/?apikey=3ca5df7&t=${searchText}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((movie) => {
+        dispatch(addMovieSearchResult(movie));
+      });
+  };
+}
+
+export function addMovieSearchResult(movie) {
+  return {
+    type: ADD_SEARCH_RESULT,
+    movie,
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import App from "./components/App";
+import rootReducer from "./reducers";
+import "./index.css";
+
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    next(action);
+  };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
